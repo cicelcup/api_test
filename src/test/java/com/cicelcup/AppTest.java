725875bdb4e238ec87f1298953846f75390f2a34
Jorge A Peroza M <cicelcup@hotmail.com>
2023-11-01 18:28:43 -0600

    Change POM to add testng

--- src/test/java/com/cicelcup/AppTest.java
+++ src/test/java/com/cicelcup/AppTest.java
@@ -1,20 +1,4 @@
 package com.cicelcup;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
-
-/**
- * Unit test for simple App.
- */
 public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
-}
+{}
