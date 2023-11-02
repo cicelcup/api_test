1cae37a8663054657f807e69d6f6ddc160b092f3
Jorge A Peroza M <cicelcup@hotmail.com>
2023-11-02 09:30:04 -0600

    Refactor People 2 Test

--- src/test/java/com/cicelcup/AppTest.java
+++ src/test/java/com/cicelcup/AppTest.java
@@ -13,23 +13,40 @@ public class AppTest
 {
     static int OK = 200;
     static String API_URL = "https://swapi.dev/api/";
+    static String peopleEndPoint = "people/2";
     
     Response response;
+    People people;
+    Gson gson = new Gson();
 
     @AfterTest
     public void tearDown()
     {
         response = null;
+        people = null;
+    }
+
+    private Response people2Response(){
+        return given().when().get(API_URL + peopleEndPoint);
     }
 
     @Test
-    public void checkPeople2Test(){
-        String endPoint = "people/2";
-        response = given().when().get(API_URL + endPoint);
+    public void checkPeople2IsOK(){
+        response = people2Response();
         Assert.assertEquals(response.getStatusCode(), OK);
+    }
 
-        Gson gson = new Gson();
-        People people = gson.fromJson(response.body().asString(), People.class);
-        System.out.println(people.toString());
+    @Test
+    public void checkPeople2Skin(){
+        response = people2Response();
+        people = gson.fromJson(response.body().asString(), People.class);
+        Assert.assertEquals(people.getSkin_color(), "gold");
+    }
+
+    @Test
+    public void checkPeopleFilms(){
+        response = people2Response();
+        people = gson.fromJson(response.body().asString(), People.class);
+        Assert.assertEquals(people.getFilms().size(), 6);
     }
 }
